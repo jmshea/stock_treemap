fe29db9510af7de57c759741e29d111eb6a3db9b
John M. Shea <jshea@ece.ufl.edu>
2021-06-21 16:04:20 -0400

    Allow user to optionally pass in cash_balance to represent value of rest of portfolio.

--- stock_treemap/__init__.py
+++ stock_treemap/__init__.py
@@ -6,7 +6,7 @@ stock tickers to sectors (strings).
 
 Also provided separate function to replot if sectors are updated (without having to wait
 for data to be pulled from Yahoo Finance, which can be quite slow). '''
-__version__ = '1.2'
+__version__ = '1.3'
 
 import yfinance as yf
 from tqdm import tqdm
@@ -15,14 +15,15 @@ import pandas as pd
 import plotly.express as px
 
 
-def stock_treemap( csv_file, sectors={} ):
+def stock_treemap( csv_file, sectors={}, cash_balance=0 ):
         '''
         Pull financial data for stocks from Yahoo finance and generate a treemap
         to illustrate performance by sectors
 
         Inputs:
         csv_file = a CSV file with columns ticker (standard stock ticker symbol) and shares (number of shares owned)
-        sectors = a dictionary that maps ticker symbols to sectors, both of which are strings
+        sectors (optional)= a dictionary that maps ticker symbols to sectors, both of which are strings
+        cash_balance (optional) = amount not in stocks that will be added to give whole portfolio balance
 
         Returns:
         df = a Pandas dataframe with the stock price, last price, market value, day's value change by stock,
@@ -68,15 +69,23 @@ def stock_treemap( csv_file, sectors={} ):
 
         df['change (day)'] = (df['price']-df['previous close'])*df['shares']
 
-        df['percent change (day)'] =             np.round((df['price']-df['previous close'])/df['previous close']*100,2)
+        df['percent change (day)'] = np.round((df['price']-df['previous close'])/df['previous close']*100,2)
 
-        total_value=df['market value'].sum()
+        if cash_balance > 0:
+                title_start="Porfolio value: &#36;"
+        else:
+                title_start="Stocks value: &#36;"
+
+
+        total_value=df['market value'].sum() + cash_balance
         if total_value<100_000:
-                my_title='Stocks value: &#36;' + str(int(np.round(total_value)))+ ", Today's change: &#36;" \
+                my_title=title_start + str(int(np.round(total_value))) + ", Today's change: &#36;" \
                         + str(int(np.round(df['change (day)'].sum())))
         else:
-                my_title='Stocks value: &#36;' + str(int(np.round(total_value/1000)))+'k' + ", Today's change: &#36;" \
+                my_title=title_start + str(int(np.round(total_value/1000))) +'k' + ", Today's change: &#36;" \
                         + str(int(np.round(df['change (day)'].sum())))
+
+
 
 
         fig = px.treemap(df, 
@@ -96,7 +105,7 @@ def stock_treemap( csv_file, sectors={} ):
 
 
 
-def update_sectors(df, sectors):
+def update_sectors(df, sectors, cash_balance=0 ):
 
     '''
     use on dataframe created by stock_treemap to update sectors for stocks and replot the treemap.
@@ -105,6 +114,7 @@ def update_sectors(df, sectors):
     inputs:
     df = a pandas dataframe created by stock_treemap (or containing all the same columns)
     sectors = a dictionary that maps ticker symbols to sectors, both of which are strings
+    cash_balance (optional) = amount not in stocks that will be added to give whole portfolio balance
 
     returns:
     df = a pandas dataframe with the stock price, last price, market value, day's value change by stock,
@@ -121,13 +131,21 @@ def update_sectors(df, sectors):
         if ticker in sectors:
             df.loc[ticker, 'sector']=sectors[ticker]
 
-    total_value=df['market value'].sum()
+    if cash_balance > 0:
+        title_start="Porfolio value: &#36;"
+    else:
+        title_start="Stocks value: &#36;"
+
+
+    total_value=df['market value'].sum() + cash_balance
     if total_value<100_000:
-        my_title='Stocks value: &#36;' + str(int(np.round(total_value)))+ ", Today's change: &#36;" \
+        my_title=title_start + str(int(np.round(total_value))) + ", Today's change: &#36;" \
                 + str(int(np.round(df['change (day)'].sum())))
     else:
-        my_title='Stocks value: &#36;' + str(int(np.round(total_value/1000)))+'k' + ", Today's change: &#36;" \
+        my_title=title_start + str(int(np.round(total_value/1000))) +'k' + ", Today's change: &#36;" \
                 + str(int(np.round(df['change (day)'].sum())))
+
+
 
     fig = px.treemap(df, 
                      path=['sector',df.index], 
